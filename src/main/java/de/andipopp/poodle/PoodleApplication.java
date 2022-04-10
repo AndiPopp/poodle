913e1e900980be2cab774ab05b77099cd2d814aa
AndiPopp <andreas.popp@gmx.de>
2022-04-10 17:54:34 +0200

    test commit

--- src/main/java/de/andipopp/poodle/PoodleApplication.java
+++ src/main/java/de/andipopp/poodle/PoodleApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * The entry point of the Spring Boot application.
- *
+ * 
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  *
